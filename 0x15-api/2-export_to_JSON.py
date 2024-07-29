d9f7e6f642a3e5b314c936ea84a3d44a13764792
KingJ89 <janmutewera@gmail.com>
2024-07-29 15:10:24 -0700

    Task 2export ammended successfully

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,18 +12,21 @@ if __name__ == '__main__':
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     username = requests.get(user_url).json().get('username')
 
+    # Fetch tasks
     tasks_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
     tasks = requests.get(tasks_url).json()
 
     # Create JSON structure
-    user_tasks = [{
-
-         user_tasks = [{
-                    "task": task['title'],
-                    "completed": task['completed'],
-                    "username": username
-                  } for task in tasks]
+    user_tasks = [
+        {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": username
+        } for task in tasks
+    ]
     user_data = {user_id: user_tasks}
 
+    # Write to JSON file
     with open(f'{user_id}.json', 'w') as file:
         json.dump(user_data, file)
+
