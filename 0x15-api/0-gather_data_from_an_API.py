cc455cabd15dfa39474ffa3adee1110b0996dfc6
KingJ89 <janmutewera@gmail.com>
2024-07-29 14:31:19 -0700

    Task 0 gather data from API complete

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # API base URL
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Get user info
+    user = requests.get(f"{url}users/{sys.argv[1]}").json()
+
+     todos = requests.get(f"{url}todos", params={"userId": sys.argv[1]}).json()
+
+    # Filter completed tasks
+    completed = [t["title"] for t in todos if t["completed"]]
+
+    print(f"Employee {user['name']} is done with tasks({len(completed)}/{len(todos)}):")
+
+    # Print completed task titles
+    for task in completed:
+        print(f"\t {task}")
