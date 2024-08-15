94c6f67d602a5bbf81f9d3d6ad4afe6f0ac21a29
KingJ89 <janmutewera@gmail.com>
2024-08-15 13:23:33 -0700

    Task ammendments completed successfully

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,7 @@
 # Ensure proper PHP configuration for WordPress by updating wp-settings.php
 
-exec { 'correct-wordpress-config':
-  command   => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path      => ['/usr/local/bin', '/bin'],
-  unless    => '/bin/grep -q "php" /var/www/html/wp-settings.php', # Run only if 'php' is not found
-  logoutput => true,  # Log command output for debugging
-  require   => File['/var/www/html/wp-settings.php'], # Ensure the file exists before running
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+  unless    => '/bin/grep -q "php" /var/www/html/wp-settings.php'
 }
-
