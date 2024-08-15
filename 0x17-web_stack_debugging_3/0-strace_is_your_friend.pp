e9cb667a73fc03fa017bf401bfd86a320f0f95ef
KingJ89 <janmutewera@gmail.com>
2024-08-15 13:20:10 -0700

    Task ammendments completed successfully

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,10 @@
-# Resolve Apache 500 Internal Server Error for WordPress
+# Ensure proper PHP configuration for WordPress by updating wp-settings.php
 
-# Define a custom Puppet resource to update the wp-settings.php file
-exec { 'update-wordpress-settings':
-  command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/bin',
-  logoutput => true,  # Ensures that output is logged for debugging
-  unless  => '/bin/grep -q "php" /var/www/html/wp-settings.php', # Checks if the change is already applied
+exec { 'correct-wordpress-config':
+  command   => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path      => ['/usr/local/bin', '/bin'],
+  unless    => '/bin/grep -q "php" /var/www/html/wp-settings.php', # Run only if 'php' is not found
+  logoutput => true,  # Log command output for debugging
+  require   => File['/var/www/html/wp-settings.php'], # Ensure the file exists before running
 }
+
