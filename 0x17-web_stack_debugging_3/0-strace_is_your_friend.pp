532d05f69677060b646c45cbe70be86441b87d0a
KingJ89 <janmutewera@gmail.com>
2024-08-15 13:24:35 -0700

    Task ammendments completed successfully

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,4 @@
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
-  unless    => '/bin/grep -q "php" /var/www/html/wp-settings.php'
 }
